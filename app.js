f15b394a508e6194de69eff2f56f30c301239ad5
IamZenDevX <zendevx.social@gmail.com>
2024-11-29 15:59:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,26 +5,16 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const indexfile = require("./router/index.js");
 const ErrorHandler = require('./utils/default/globalErrorHandler.js');
-
-
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
-
 // Load environment variables from .env file
 dotenv.config();
-
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
 app.use("/", indexfile);  
 
 const dbURI = `mongodb+srv://name:password@cluster0.0tq4xsw.mongodb.net/bms?retryWrites=true&w=majority&appName=Cluster0`;
